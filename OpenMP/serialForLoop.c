5771190945303f6856287eac2d0006a794f2802f
zaid <zald0001@student.monash.edu>
2022-10-27 19:39:44 +0800

    openMP and Threads

---
+++ OpenMP/serialForLoop.c
@@ -0,0 +1,10 @@
+// Serial Code
+#include <stdio.h>
+
+void main() {
+    int i;
+    printf("Hello World\n");
+    for (i = 0; i < 6; i++)
+        printf("Iter:%d\n", i);
+    printf("GoodBye World\n");
+}
