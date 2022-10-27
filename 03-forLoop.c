ce5fa2a43fc253063b178343a61d1249b8c16fa9
zaid <80106523+0xzaid@users.noreply.github.com>
2022-10-27 17:15:52 +0800

    Add files via upload

---
+++ 03-forLoop.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(){
+	
+	for(int i=1; i <= 100; i++){
+		printf("number: %d\n", i);	
+	}
+
+	return 0;
+}
+
+
---
+++ 05-Conditionals1.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(){
+	
+	for(int i=1; i<=100; i++){
+		if(i%2==0){
+			printf("Even number: %d\n", i);	
+		}
+	}
+	return 0;
+}
+
+
